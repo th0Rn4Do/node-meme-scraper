7f18687f905b0c6ee7f474ad9ab0c426fcf68033
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-04-27 15:39:14 +0200

    Add 10 logs  where to find the first 10 URLs in the html-string using indexOf

--- index.js
+++ index.js
@@ -22,15 +22,93 @@ await fetch('https://memegen-link-examples-upleveled.netlify.app/')
 // fetch('').then((response) => then.)...
 // check recording time: 12:10
 
+// This looks for the substring 'Scipt' as a test
 console.log(testSubstringForIndexOf.indexOf('Script'));
-const positionOfFirstImagesUrl = htmlFromMemeMainpage.indexOf('<img src');
-console.log(positionOfFirstImagesUrl);
+
+// This looks for the 1st substring <img src
+const positionOf1stImageUrl = htmlFromMemeMainpage.indexOf('<img src');
+console.log('1st URL');
+console.log(positionOf1stImageUrl);
+
+// This looks for the 2st substring <img src
+const positionOf2ndImageUrl = htmlFromMemeMainpage.indexOf(
+  '<img src',
+  positionOf1stImageUrl + 1,
+);
+console.log('2nd URL');
+console.log(positionOf2ndImageUrl);
+
+// This looks for the 3rd substring <img src
+const positionOf3rdImagesUrl = htmlFromMemeMainpage.indexOf(
+  '<img src',
+  positionOf2ndImageUrl + 1,
+);
+console.log('3rd URL');
+console.log(positionOf3rdImagesUrl);
+
+// This looks for the 4th substring <img src
+const positionOf4thImagesUrl = htmlFromMemeMainpage.indexOf(
+  '<img src',
+  positionOf3rdImagesUrl + 1,
+);
+console.log('4th URL');
+console.log(positionOf4thImagesUrl);
+
+// This looks for the 5th substring <img src
+const positionOf5thImagesUrl = htmlFromMemeMainpage.indexOf(
+  '<img src',
+  positionOf4thImagesUrl + 1,
+);
+console.log('5th URL');
+console.log(positionOf5thImagesUrl);
+
+// This looks for the 6th substring <img src
+const positionOf6thImagesUrl = htmlFromMemeMainpage.indexOf(
+  '<img src',
+  positionOf5thImagesUrl + 1,
+);
+console.log('6th URL');
+console.log(positionOf6thImagesUrl);
+
+// This looks for the 7th substring <img src
+const positionOf7thImagesUrl = htmlFromMemeMainpage.indexOf(
+  '<img src',
+  positionOf6thImagesUrl + 1,
+);
+console.log('7th URL');
+console.log(positionOf7thImagesUrl);
+
+// This looks for the 8th substring <img src
+const positionOf8thImagesUrl = htmlFromMemeMainpage.indexOf(
+  '<img src',
+  positionOf7thImagesUrl + 1,
+);
+console.log('8th URL');
+console.log(positionOf8thImagesUrl);
+
+// This looks for the 9th substring <img src
+const positionOf9thImagesUrl = htmlFromMemeMainpage.indexOf(
+  '<img src',
+  positionOf8thImagesUrl + 1,
+);
+console.log('9th URL');
+console.log(positionOf9thImagesUrl);
+
+// This looks for the 10th substring <img src
+const positionOf10thImagesUrl = htmlFromMemeMainpage.indexOf(
+  '<img src',
+  positionOf9thImagesUrl + 1,
+);
+console.log('10th URL');
+console.log(positionOf10thImagesUrl);
 
 /*
 const positionOfFirstImagesUrl =
 const firstImagesUrl = htmlFromMemeMainpage.substring(7990, 8100);
 console.log(stuff2);
+*/
 
+/*
 const secondImagesUrl = htmlFromMemeMainpage.substring(7990, 8100);
 console.log(stuff2);
 
