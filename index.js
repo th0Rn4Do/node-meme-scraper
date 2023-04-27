11fe419f0a4e7bce37a90416c9bc253bdfba857b
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-04-27 15:20:13 +0200

    Add a way to find the first URL

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 import fetch from 'node-fetch';
 
+// This output just a test output to check wether the program is running at all.
 console.log("What's up!!");
 
-const javastuff = 'JavaScript';
+// This const is a test string to test wether the substring can be found later in the program
+const testSubstringForIndexOf = 'JavaScript';
+
+// This const is a test to know that stringForHtml was initialised with abc
 const stringForHtml = 'abc';
 console.log(stringForHtml);
 
-let htmlFromMemeMainpage;
+// This initialises the htmlFromMemeMainpage in order to assign the retured html-string later
+let htmlFromMemeMainpage = 'no return html';
 
+// This is a fetch-method wich is built in JavaScript and it fetches the https protocol from a website and converts them to a text and assigns it to htmlFromMemeMainpage
 await fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((response) => response.text())
   .then((data) => (htmlFromMemeMainpage = data));
 
+// This is the initail note I made what the fetch-method looks like that I need for this project
 // fetch('').then((response) => then.)...
 // check recording time: 12:10
 
-console.log(javastuff.indexOf('Script'));
-console.log(htmlFromMemeMainpage.indexOf('<img src'));
-const stuff2 = htmlFromMemeMainpage.substring(7990, 8100);
+console.log(testSubstringForIndexOf.indexOf('Script'));
+const positionOfFirstImagesUrl = htmlFromMemeMainpage.indexOf('<img src');
+console.log(positionOfFirstImagesUrl);
+
+/*
+const positionOfFirstImagesUrl =
+const firstImagesUrl = htmlFromMemeMainpage.substring(7990, 8100);
 console.log(stuff2);
+
+const secondImagesUrl = htmlFromMemeMainpage.substring(7990, 8100);
+console.log(stuff2);
+
+const firstImagesUrl = htmlFromMemeMainpage.substring(7990, 8100);
+console.log(stuff2);
+*/
